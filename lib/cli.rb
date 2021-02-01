fe8e5770a63f6d923b3542aa6aa693246bc3cb0e
Aly Klepper <alyklepper1@gmail.com>
2021-01-31 16:10:52 -0800

    adds welcome method

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,31 @@ class Cli
 
 
 def welcome
-    puts "hi"
+    puts "Welcome, space explorer!"
+    puts <<-'explorer'
+      _..._
+    .'     '.      _
+   /    .-""-\   _/ \
+ .-|   /:.   |  |   |
+ |  \  |:.   /.-'-./
+ | .-'-;:__.'    =/
+ .'=  *=|NASA _.='
+/   _.  |    ;
+;-.-'|    \   |
+/   | \    _\  _\
+\__/'._;.  ==' ==\
+       \    \   |
+       /    /   /
+       /-._/-._/
+       \   `\  \
+        `-._/._/
+explorer
+    puts ""
+    puts "We are glad to have you on board!"
+    puts ""
+    puts ""
+    sleep(2)
+    
 end
 
 end # end of class
