1a714cb07c7c05782aba4a571d8faff5ae28561d
Aly Klepper <alyklepper1@gmail.com>
2021-02-01 19:52:50 -0800

    did main_menu and refactored start and info_or_fact. Bind.pry 2morrow. Im tired

--- lib/cli.rb
+++ lib/cli.rb
@@ -36,24 +36,38 @@ def start
         puts "#{p.name}"
     end
     input = gets.strip.downcase
-    if input == #{p.name}
+    if input == p.name
         self.info_or_fact
     else
         puts "Houston, we have a problem. Type a planet again, explorer!"
+        self.start
     end
 end
 
-def info_or_fact
-    puts "Explorer, would you like a fact or information on your chosen planet? Select the number"
-    Space.all.each.with_index(1) do |p, i| 
-        puts "#{i}.) #{p.fun_fact
-    end
-    
-    self.main_menu
-end
+#def info_or_fact
+    #puts "Explorer, would you like a fact or information on your chosen planet? Enter the number:"
+    #Space.all.each.with_index(1) do |p, i| 
+        #puts "#{i}.) #{p.fun_fact}
+    #end
+    #input = gets.strip.downcase
+    #if input == "1" 
+        #{i}.) #{p.fun_fact}
+    #elsif input == "2"
+    ##{i}.) #{p.fun_fact}
+    #self.main_menu
+    #else
+    # puts "Are you speaking alien? Enter number again, explorer!
+    #end"
+#end
 
 def main_menu
-   
+   puts "Would you like to see the list again? y or n"
+   input = gets.strip.downcase
+   if input == "y"
+    self.start
+   else
+    self.exit
+   end
 end
 
 def exit
